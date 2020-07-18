7d31aaa973f8801fa0afc12ac679d6d7d0044fd9
Catherine <catherineep4189@gmail.com>
2020-07-18 22:44:54 +0100

    Testing random integer number in given range linked to id numbers for Marvel Characters

--- Marvel_Character.py
+++ Marvel_Character.py
@@ -1,7 +1,17 @@
 import ItsHidden
+import requests
+import random
 
 key = ItsHidden.api_key
 hash_key = ItsHidden.hashkey
+
+generateNumber = random.randint(1009000, 1009999)
+
+print(generateNumber)
+
+url = f"https://gateway.marvel.com/v1/public/characters?id={generateNumber}&ts=567&apikey={key}&hash={hash_key}"
+
+
 
 
 
